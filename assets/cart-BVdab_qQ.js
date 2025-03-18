58d43eae0ee70f4cb23c83cd780b8d5931cd148f
paulomath28 <67445880+paulomath28@users.noreply.github.com>
2025-03-17 22:01:23 -0300

    Update cart-BVdab_qQ.js

--- assets/cart-BVdab_qQ.js
+++ assets/cart-BVdab_qQ.js
@@ -1,1 +1,14 @@
-
+import{u as o,g as s}from"./utils-DUQbpE7m.js";function n(){const r=s("so-cart").map(t=>l(t));document.querySelector(".product-list").innerHTML=r.join("")}function l(a){var c,e;const r=((e=(c=a.Colors)==null?void 0:c[0])==null?void 0:e.ColorName)||"No Color";return`<li class="cart-card divider">
+    <a href="#" class="cart-card__image">
+      <img
+        src="${a.Image}"
+        alt="${a.Name}"
+      />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${a.Name}</h2>
+    </a>
+    <p class="cart-card__color">${r}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${a.FinalPrice}</p>
+  </li>`}n();o();
